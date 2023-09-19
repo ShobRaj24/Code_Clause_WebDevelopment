f6ce21ed0b3050723b9b164963d1ea473ce6e004
SHOB RAJ GOWDA K <94729553+ShobRaj24@users.noreply.github.com>
2023-09-19 12:39:57 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+
+app.use(express.static('public'));
+
+// WebSocket connection handler
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    console.log(`Received: ${message}`);
+    
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
